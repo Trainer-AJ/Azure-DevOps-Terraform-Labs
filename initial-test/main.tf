f5254242ca7e0de8f36f0dc929da89578fe4c551
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2024-01-01 19:02:33 +0530

    Create main.tf

---
+++ initial-test/main.tf
@@ -0,0 +1,57 @@
+# ----------------------------------------------------
+# LAB: 1 SetUp Terraform 
+# AUTHOR: Ananay Ojha
+# ---------------------------------------------------
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+  # required_version = "value"
+}
+
+# AFter AZ Login command, Create a service Principal In Azure Using below Command
+# az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/20000000-0000-0000-0000-000000000000"
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+# ----------------------------------------------
+# Now You Can Create Resources Here
+
+# CREATE A RESOURCE GROUP
+
+resource "azurerm_resource_group" "first" {
+  provider = azurerm.azure-pass
+  name     = "RG-1"
+  location = "EastUS"
+}
+
+resource "azurerm_resource_group" "second" {
+  provider = azurerm.azure-pass
+  name     = "RG-2"
+  location = "Central India"
+}
+data "azurerm_resource_group" "example-1" {
+  provider = azurerm.azure-pass
+  name     = azurerm_resource_group.first.name
+}
+
+data "azurerm_resource_group" "example-2" {
+  depends_on = [azurerm_resource_group.second]
+  provider   = azurerm.azure-pass
+  name       = "RG-2"
+}
+
+output "id" {
+  value = " The First Resource group's name is ${azurerm_resource_group.first.name} & ID is ${data.azurerm_resource_group.example-1.id}"
+}
+
+output "RG2-id" {
+  depends_on = [azurerm_resource_group.second]
+  value      = data.azurerm_resource_group.example-2
+}
